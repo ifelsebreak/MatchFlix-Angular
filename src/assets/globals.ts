29976d317836febd06f98283afe686c6ac9a5cc1
Alberto <42868392+ifelsebreak@users.noreply.github.com>
2021-12-14 22:08:57 +0000

    Update globals.ts

--- src/assets/globals.ts
+++ src/assets/globals.ts
@@ -1,3 +1,3 @@
-export const api_key: string = '197c3e3ad86e0b0c080d2373b01603e0';
+export const api_key: string = 'PUT YOUR themoviedb.org API KEY HERE';
 export var language: string = 'en-US';
 export var isNeeded: boolean;
